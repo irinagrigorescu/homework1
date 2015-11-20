c1ae0ad84b3cfb46b4cb1f6cd6fa4a7379f580f5
irinagry <irinagry@gmail.com>
2015-11-20 16:16:51 +0000

    Changed arguments and added dest option

--- greengraph/command.py
+++ greengraph/command.py
@@ -5,17 +5,18 @@ from greengraph import Greengraph
 def process():
     parser = ArgumentParser(description = "Generating a greengraph by using a start point, an end point, number of steps and the name of the file you want the graph to be stored at.")
     
-    parser.add_argument('--froml', '-f')
-    parser.add_argument('--to', '-t')
+    parser.add_argument('--from', '-f', dest = 'fromCity')
+    parser.add_argument('--to', '-t', dest = 'toCity')
     parser.add_argument('--steps', '-s')
     parser.add_argument('--out', '-o')
 
     args = parser.parse_args()
-    
-    myGraph = Greengraph(args.froml, args.to)
+
+    myGraph = Greengraph(args.fromCity, args.toCity)
     data = myGraph.green_between(args.steps)
     plt.plot(data)
     plt.savefig(args.out)
+    
 
 if __name__ == "__main__":
     process()
