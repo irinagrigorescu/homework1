d64fa6d740ab90969ca83de8ff7dcb438ca9c306
irinagry <irinagry@gmail.com>
2016-01-06 12:53:03 +0000

    added more fields to setup script file

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Greengraph",
-    version = "0.1",
+    version = "1.0",
+    description = "Greengraph Assignment",
+    author = "Irina Grigorescu",
+    author_email = "irina.grigorescu.15@ucl.ac.uk",
+    url = "https://github.com/irinagrigorescu/homework1",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
     install_requires = ['argparse', 'pypng', 'numpy', 'geopy', 'matplotlib', 'requests']
