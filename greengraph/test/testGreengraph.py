470dc3c4ff4dfe9ebb0d40a753deb1310c63b37a
irinagry <irinagry@gmail.com>
2016-01-10 17:21:01 +0000

    Added code for test_location_sequence

--- greengraph/test/testGreengraph.py
+++ greengraph/test/testGreengraph.py
@@ -1,23 +1,54 @@
+import os
 import yaml
+import numpy as np
 import unittest
 from ..greengraph import Greengraph
 from nose.tools import *
 from mock import Mock, patch
 
-class TestGreengraph(unittest.Testcase):
+'''
+Class for testing Greengraph
+'''
+class TestGreengraph(unittest.TestCase):
     
     def setUp(self):
+        '''
+        Loading fixtures from samplesGreengraph.yaml file
+        '''
         with open(os.path.join(os.path.dirname(__file__), 'fixtures', 'samplesGreengraph.yaml')) as fixture_file:
             self.fixtures = yaml.load(fixture_file)
 
     def test_geolocate(self):
-        pass
+        '''
+        Going through all the testcases for the geolocate method
+        '''
+        for testcase in self.fixtures['geolocate']:
+            location = testcase.pop('location')
+            answer = (testcase.pop('anslat'), testcase.pop('anslong'))
+            
+            geo_attr = {'geocoder.geocode.return_value' : answer}
+            geo = Mock(**geo_attr)
+            assert_equal(geo.geocoder.geocode(location), answer)
 
     def test_location_sequence(self):
-        pass
+        '''
+        Going through all the testcases for the location_sequence method
+        '''
+        for testcase in self.fixtures['location_sequence']:
+            start = tuple(testcase.pop('start'))
+            end = tuple(testcase.pop('end'))
+            steps = testcase.pop('steps')
+            answer = np.vstack(testcase.pop('answer'))
+            
+            geo = Greengraph(0, 0)
+            return_val = geo.location_sequence(start, end, steps)
+            assert(np.array_equal(return_val, answer))
 
     def test_green_between(self):
+        '''
+        pass
+        '''
         pass
 
-in __name__ == "__main__":
+if __name__ == "__main__":
     unittest.main()
