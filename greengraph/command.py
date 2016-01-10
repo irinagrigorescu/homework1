b7a94c2be2d020b0ee03623fad97306a8a676ab3
irinagry <irinagry@gmail.com>
2016-01-10 17:19:31 +0000

    added comments to command.py

--- greengraph/command.py
+++ greengraph/command.py
@@ -3,15 +3,23 @@ from argparse import ArgumentParser
 from matplotlib import pyplot as plt
 from greengraph import Greengraph 
 
+'''
+Command line entry point
+'''
 def process():
     parser = ArgumentParser(description = "Generating a greengraph by using a start point, an end point, number of steps and the name of the file you want the graph to be stored at.")
     
+    '''
+    Parameters
+    '''
     parser.add_argument('--from', '-f', dest = 'fromCity')
     parser.add_argument('--to', '-t', dest = 'toCity')
     parser.add_argument('--steps', '-s')
     parser.add_argument('--out', '-o')
 
-    # Print help message even if no flag is provided
+    '''
+    Print help message even if no flag is provided
+    '''
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
